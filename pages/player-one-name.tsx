a6d0448bac630468b45bcbd55297bb85dda1efcc
Minhajul Hoq <minhoq@gmail.com>
2022-03-24 17:17:13 -0400

    page update

---
+++ pages/player-one-name.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import React, { useEffect } from "react";
+import { styled } from "../stitches.config";
+
+const Strong = styled("strong", {
+  fontSize: 50,
+  color: "#fff",
+});
+const Page: NextPage = () => {
+  const [data, setData] = React.useState<string>("");
+  const loadData = async () => {
+    const playerOneResponse = await fetch("/api/player-one");
+    const playerOneData = playerOneResponse.json();
+    const playerOne = await playerOneData;
+    setData(playerOne.name);
+  };
+  useEffect(() => {
+    loadData();
+  }, []);
+  return (
+    <div>
+      <Head>
+        <title>MTG Score Keeper</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Strong>{data}</Strong>
+      </main>
+    </div>
+  );
+};
+
+export default Page;
---
+++ pages/player-two-name.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import React, { useEffect } from "react";
+import { styled } from "../stitches.config";
+
+const Strong = styled("strong", {
+  fontSize: 50,
+  color: "#fff",
+});
+const Page: NextPage = () => {
+  const [data, setData] = React.useState<string>("");
+  const loadData = async () => {
+    const playerTwoResponse = await fetch("/api/player-two");
+    const playerTwoData = playerTwoResponse.json();
+    const playerTwo = await playerTwoData;
+    setData(playerTwo.name);
+  };
+  useEffect(() => {
+    loadData();
+  }, []);
+  return (
+    <div>
+      <Head>
+        <title>MTG Score Keeper</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Strong>{data}</Strong>
+      </main>
+    </div>
+  );
+};
+
+export default Page;
